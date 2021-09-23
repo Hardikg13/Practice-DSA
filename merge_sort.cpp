be1558588995da6aeae7e730d6708ccf63fc8dd8
Hardik Gupta <29226479+harryg13@users.noreply.github.com>
2021-09-23 16:51:14 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -6,19 +6,16 @@ void merge(vector<int> &array,int s,int e){
     int i = s;
     int m =(s+e)/2;
     int j = m+1;
-
     vector<int> temp;
     while ((i<=m) and (j<=e))
     {
         if(array[i]<array[j]){
             temp.push_back(array[i]);
             i++;
-
         }
         else{
             temp.push_back(array[j]);
             j++;
-
         }
     }
     
@@ -46,8 +43,6 @@ void merge_sort(vector<int> &arr,int s,int e){
     merge_sort(arr,mid+1,e);
     return merge(arr,s,e);
 }
-
-
 int main(){
     vector<int> arr{10,5,2,0,7,6,4};
     int s=0;
