532753a2cd1af9d0e6777e4aeaeb75b131f851c7
Hardik Gupta <29226479+harryg13@users.noreply.github.com>
2021-09-24 17:19:25 +0530

    Add files via upload

---
+++ Finding_subsets.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void finding_subsets(char *input,char *output, int i , int j){
+    if(input[i] =='\0'){
+        output[j] = '\0';
+        cout<<output<<endl;
+        return;
+
+    }
+     output[j]= input[i];
+     finding_subsets(input,output,i+1,j+1);
+     finding_subsets(input,output,i+1,j);
+
+}
+int main(){
+    char input[100];
+    char output[100];
+    cin>>input;
+    finding_subsets(input,output,0,0);
+    return 0;
+
+}
